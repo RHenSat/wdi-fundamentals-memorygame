d2557f723c0bb4ef50338b2b1a25616bec5c4f3a
R. Sebastien Henri-Saturne <sebastien@Rs-MacBook-Air.local>
2020-02-21 11:45:57 -0500

    creat cool game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,18 +11,71 @@ const Cards = [ "queen","queen","king","king"];
 
 const cardsInPlay = [];
 
-cardOne = cards [0];
-
-cardsInplay.push(cardOne);
-console.log('User flipped' + cardOne);
-
-cardTwo = cards [2]
-
-cardsInplay.push(cardTwo);
-console.log('User flipped' + cardTwo);
 
 if (cardsInPlay.length === 2) { (cardsInPlay [0] === cardsInplay [1]); { alert ("You found a match!"); }
  { alert ("Sorry, try again.")}
 }
 
+function flipCard (cardID) { console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king"
+
+console.log("User flipped" + cardOne);
+console.log("User flipped"+ cardThree);
+}
+
+
+console.log ("User flipped card" + card [cardID])
+console.log {"cards" (cardID)}
+
+const cardsInplay = []
+
+function checkforMatch () {
+	if (cardsInplay [0] === cardsInplay [1]) {
+		alert ("You found a match!");
+	} else {
+		alert ("Sorry, try again.");
+	}
+}
+
+{rank:"queen",
+suit: "hearts",
+cardImage: "image/queenofhearts/png."
+};
+
+{rank:"queen"
+suit: "diamonds"
+cardImage: "image/queenofdiamonds/png."
+};
+{
+rank:"king",
+suit: "hearts",
+cardImage: "image/kingofhearts/png."
+};
+
+rank:"king"
+suit: "diamonds"
+cardImage: "image/kingofdiamonds/png."
+
+
+function flipCard () {}
+const cardId = this.getAttribute ('data-iD');
+console.log ("User flipped" + cards [cardID].rank);
+console.log (card [cardID].cardImage);
+console.log (card [cardID].suit);
+cardsInplay.push (cards[cardID].rank);
+this.setAttribute (src. cards.[cardsID].cardImage);
+if (cards.length === 2) {checkforMatch ();}
+
+function createBoard () {
+	for (let i = 0; i < cards.length; i++) { 
+		let cardElement = document.createElement ('img');
+		cardElement.setAttribute ('src', 'images/back.png');
+		cardElement.setAttribute ('data-id', i);
+		cardElement. addEventListener ('click' flipcard);
+		document.getElementbyID ('gameboard'). appendChild (cardelement);
+
+createBoard ();
 
